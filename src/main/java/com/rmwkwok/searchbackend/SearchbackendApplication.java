de19fdf913504b0cdd75f395e7981e479012b89a
rmwkwok <rmwkwok@gmail.com>
2021-02-28 09:47:55 +0800

    fix: handle argument properly with commandlineparser

--- src/main/java/com/rmwkwok/searchbackend/SearchbackendApplication.java
+++ src/main/java/com/rmwkwok/searchbackend/SearchbackendApplication.java
@@ -1,7 +1,10 @@
 package com.rmwkwok.searchbackend;
 
+import org.apache.commons.cli.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 // https://start.spring.io/ - generated this template
 
@@ -18,37 +21,50 @@ public class SearchbackendApplication {
 	static boolean doQueryModification = true;
 
 	public static void main(String[] args) {
-		parseArgs(args);
-		SpringApplication.run(SearchbackendApplication.class, args);
-	}
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLineParser parser = new DefaultParser();
 
-	public static void parseArgs(String[] args) {
-		for (String arg: args) {
-			System.out.println("---------------------------------------------------------------");
-			System.out.println(arg);
-			// Option Arguments
-			if (arg.startsWith("--") && arg.contains("=")) {
-				String pKey = arg.substring(2, arg.indexOf("="));
-				String pVal = arg.substring(arg.indexOf("=") + 1);
-				switch (pKey) {
-					case "w2vFolder": w2vFolder = pVal;
-						break;
-					case "indexFolder": indexFolder = pVal;
-						break;
-					case "numSearchResult": numSearchResult = Integer.parseInt(pVal);
-						break;
-					case "numExtraSearchResult": numExtraSearchResult = Integer.parseInt(pVal);
-						break;
-					case "minSnippetWords": minSnippetWords = Integer.parseInt(pVal);
-						break;
-					case "closeCoOccurrenceCondition": closeCoOccurrenceCondition = Integer.parseInt(pVal);
-						break;
-					case "doQueryExpansion": doQueryExpansion = Integer.parseInt(pVal) > 0;
-						break;
-					case "doQueryModification": doQueryModification = Integer.parseInt(pVal) > 0;
-						break;
-				}
+		Options options = new Options();
+		options.addOption(Option.builder("h").longOpt("help").build());
+		options.addOption(Option.builder().required().type(File.class).hasArg().longOpt("indexFolder").argName("folder").desc("Lucene index folder").build());
+		options.addOption(Option.builder().required().type(File.class).hasArg().longOpt("w2vFolder").argName("folder").desc("w2v index folder").build());
+		options.addOption(Option.builder().type(Number.class).hasArg().longOpt("numSearchResult").argName(String.valueOf(numSearchResult)).desc("Number of search results to return").build());
+		options.addOption(Option.builder().type(Number.class).hasArg().longOpt("numExtraSearchResult").argName(String.valueOf(numExtraSearchResult)).desc("Number of extra search results to consider before returning the top results").build());
+		options.addOption(Option.builder().type(Number.class).hasArg().longOpt("minSnippetWords").argName(String.valueOf(minSnippetWords)).desc("Minimum number of words in considering the best snippet").build());
+		options.addOption(Option.builder().type(Number.class).hasArg().longOpt("closeCoOccurrenceCondition").argName(String.valueOf(closeCoOccurrenceCondition)).desc("How close in terms of number of words to consider as close co-occurrence").build());
+		options.addOption(Option.builder().type(Number.class).hasArg().longOpt("doQueryExpansion").argName(doQueryExpansion ? "1" : "0").desc("Whether to do expand query with similar words or not. Positive integer to enable").build());
+		options.addOption(Option.builder().type(Number.class).hasArg().longOpt("doQueryModification").argName(doQueryModification ? "1" : "0").desc("Whether to modify query word with document id or not. Positive integer to enable").build());
+
+		try {
+			CommandLine cmd = parser.parse(options, args);
+
+			if(cmd.hasOption( "help" )) {
+				formatter.printHelp("searcher", options);
+				return;
 			}
+
+			w2vFolder = ((File)cmd.getParsedOptionValue("w2vFolder")).getAbsolutePath();
+			indexFolder = ((File)cmd.getParsedOptionValue("indexFolder")).getAbsolutePath();
+
+			if(cmd.hasOption( "numSearchResult" ))
+				numSearchResult = ((Number)cmd.getParsedOptionValue("numSearchResult")).intValue();
+			if(cmd.hasOption( "numExtraSearchResult" ))
+				numExtraSearchResult = ((Number)cmd.getParsedOptionValue("numExtraSearchResult")).intValue();
+			if(cmd.hasOption( "minSnippetWords" ))
+				minSnippetWords = ((Number)cmd.getParsedOptionValue("minSnippetWords")).intValue();
+			if(cmd.hasOption( "closeCoOccurrenceCondition" ))
+				closeCoOccurrenceCondition = ((Number)cmd.getParsedOptionValue("closeCoOccurrenceCondition")).intValue();
+			if(cmd.hasOption( "doQueryExpansion" ))
+				doQueryExpansion = ((Number)cmd.getParsedOptionValue("doQueryExpansion")).intValue() > 0;
+			if(cmd.hasOption( "doQueryModification" ))
+				doQueryModification = ((Number)cmd.getParsedOptionValue("doQueryModification")).intValue() > 0;
 		}
+		catch( ParseException e ) {
+			System.out.println(e.getMessage());
+			formatter.printHelp( "searcher", options );
+			return;
+		}
+
+		SpringApplication.run(SearchbackendApplication.class, args);
 	}
 }

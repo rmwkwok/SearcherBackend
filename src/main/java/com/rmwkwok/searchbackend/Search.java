0b01d3496e466862671182fffb2b7fd2e32db4de
rmwkwok <rmwkwok@gmail.com>
2021-03-04 14:22:26 +0800

    fix: optimal snippet

--- src/main/java/com/rmwkwok/searchbackend/Search.java
+++ src/main/java/com/rmwkwok/searchbackend/Search.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/search")
@@ -152,50 +153,48 @@ public class Search {
         // For each seed, it and its n neighbour form a snippet window of n+1 matches
         // The seed with window of smallest length becomes seed for round n+2
         // It keeps iterate until length >= [minSnippetWords]
-        if (termPositions.size() > 0) {
-            int n = 0;
-            Map<Integer, Integer> seed = new HashMap<>();
-            for (int i = 0; i < termPositions.size(); i++)
-                seed.put(i, 1);
+        if (termPositions.size() > 1) {
 
-            int minLength;
-            do {
-                n++;
-                Map<Integer, Integer> temp = new HashMap<>();
+            int posStart = 0;
+            int posOrder = 0;
 
-                for (Map.Entry<Integer, Integer> entry : seed.entrySet())
-                    for (int index : new int[]{entry.getKey() - 1, entry.getKey()})
-                        if ((index >= 0) && (index + n < termPositions.size()))
-                            temp.put(index, termPositions.get(index + n).position - termPositions.get(index).position + 1);
+            // go higher order and look for better window
+            for (int winOrder = 1; winOrder < termPositions.size() - 1; winOrder++) {
+                int winLen = Integer.MAX_VALUE;
+                int winStart = -1;
+                for (int start = 0; start + winOrder < termPositions.size(); start++) {
+                    int winDiff = termPositions.get(start + winOrder).position - termPositions.get(start).position + 1;
+                    if (winDiff >= minSnippetWords && winDiff <= maxSnippetWords && winDiff < winLen) {
+                        winLen = winDiff;
+                        winStart = start;
+                    }
+                }
 
-                seed.clear();
+                int posLen = termPositions.get(posStart + posOrder).position - termPositions.get(posStart).position + 1;
+                if (winStart == -1)
+                    break;
+                if (posLen < minSnippetWords || posLen > maxSnippetWords || (winOrder+1.)/winLen >= (posOrder+1.)/posLen) {
+                    posStart = winStart;
+                    posOrder = winOrder;
+                }
+            }
 
-                minLength = temp.values().stream().min(Integer::compare).orElse(minSnippetWords);
-                for (Map.Entry<Integer, Integer> entry : temp.entrySet())
-                    if (entry.getValue() == minLength)
-                        seed.put(entry.getKey(), entry.getValue());
-
-            } while (minLength < minSnippetWords);
-
-            // i and j are beginning and ending index for matchedLocations
-            // need them to get index of contentTerms
-            int i = seed.keySet().stream().min(Integer::compare).orElse(0);
-            int j = Math.min(i + n, termPositions.size() - 1);
-
-            while (termPositions.get(j).position - termPositions.get(i).position >= maxSnippetWords)
-                j-=1;
-
-            String snippet = content.substring(termPositions.get(i).startOffset, termPositions.get(j).endOffset);
-            System.out.println("getSnippet:: length: " + snippet.length());
-            return "... " + snippet + " ...";
+            if (posStart != 0 || posOrder != 0) {
+                String snippet = content.substring(termPositions.get(posStart).startOffset, termPositions.get(posStart + posOrder).endOffset);
+                System.out.println("getSnippet:: length: " + snippet.length());
+                return "... " + snippet + " ...";
+            }
+        }
+        else if (termPositions.size() == 1) {
+            content = content.substring(termPositions.get(0).startOffset);
         }
         else {
             System.out.println("getSnippet:: No matched words");
-            return String.join("",
-                    Arrays.stream(content.split("((?<=[^a-zA-Z0-9])|(?=[^a-zA-Z0-9]))"))
-                            .limit(minSnippetWords)
-                            .collect(Collectors.toList())) + " ...";
         }
+        return String.join("",
+                Arrays.stream(content.split("((?<=[^a-zA-Z0-9])|(?=[^a-zA-Z0-9]))"))
+                        .limit(minSnippetWords)
+                        .collect(Collectors.toList())) + " ...";
     }
 
     /**
